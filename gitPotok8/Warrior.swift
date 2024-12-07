cecb7666e4bfd8e9365efa4da9affb475e96c1fe
Shamil <shamil.aglarov@gmail.com>
2024-12-07 14:50:07 +0300

    Добавил xcuserdata в .gitignore и обновил Warrior.swift

--- gitPotok8/Warrior.swift
+++ gitPotok8/Warrior.swift
@@ -5,3 +5,15 @@
 //  Created by Shamil Aglarov on 07.12.2024.
 // test
 
+class Warrior: Fighter {
+
+    override init(name: String,
+                  health: Int = 500,
+                  strength: Int = 8) {
+        super.init(name: name)
+    }
+
+    func ultimateAbility() -> Int {
+        Int.random(in: 10...20)
+    }
+}
