6b3d2d2ec48d80bad85f1311b9b9c295ef452c07
Shamil <shamil.aglarov@gmail.com>
2024-12-07 14:25:11 +0300

    created Fighter class

--- gitPotok8/Fighter.swift
+++ gitPotok8/Fighter.swift
@@ -5,3 +5,17 @@
 //  Created by Shamil Aglarov on 07.12.2024.
 //
 
+class Fighter {
+    let name: String
+
+    var health: Int
+
+    var strength: Int
+
+    init(name: String, health: Int = 100, strength: Int = 5) {
+        self.name = name
+        self.health = health
+        self.strength = strength
+    }
+
+}
