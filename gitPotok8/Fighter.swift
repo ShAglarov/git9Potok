e060f210eee71483e221c865e34e3eb0a55ae58e
ShAglarov <101290773+ShAglarov@users.noreply.github.com>
2024-12-07 15:02:00 +0300

    Update Fighter.swift

--- gitPotok8/Fighter.swift
+++ gitPotok8/Fighter.swift
@@ -8,14 +8,8 @@
 class Fighter {
     let name: String
 
-    var health: Int
-
-    var strength: Int
-
     init(name: String, health: Int = 100, strength: Int = 5) {
-        self.name = name
-        self.health = health
-        self.strength = strength
+
     }
 
 }
