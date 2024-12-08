cf8e1585e5b2f1254b7188380601ca153732c20a
Shamil <shamil.aglarov@gmail.com>
2024-12-08 08:44:53 +0300

    added main menu

--- gitPotok8/main.swift
+++ gitPotok8/main.swift
@@ -7,6 +7,12 @@
 
 import Foundation
 
-print("Hello, World!")
+var player1 = Warrior(name: "Shamil")
+var player2 = Warrior(name: "Oleg")
 
+<<<<<<< HEAD
 print("Hello iOS programmer")
+=======
+print(player1.ultimateAbility())
+print(player2.ultimateAbility())
+>>>>>>> f2cd850 (added main menu)
